b3568d95bbaa8c340313d60235e2f603e8ab577f
Ajmalniz <128520849+Ajmalniz@users.noreply.github.com>
2023-12-26 23:14:46 +0500

    file name

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import AcmeLogo from '@/app/ui/acme-logo';
 import { ArrowRightIcon } from '@heroicons/react/24/outline';
 import Link from 'next/link';
 import style from "@/app/ui/home.module.css"
-import { lusitana } from '@/app/ui/font'
+import { lusitana } from '@/app/ui/fonts'
 import Image from 'next/image';
 export default function Page() {
   return (
